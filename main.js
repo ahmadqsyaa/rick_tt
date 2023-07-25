9d95ad42085276a8601b21bebf2c7df6e56637d4
ahmadqsyaa <114840842+ahmadqsyaa@users.noreply.github.com>
2023-07-26 05:34:26 +0700

    update main.js

--- main.js
+++ main.js
@@ -27,24 +27,24 @@ module.exports = {
   },
   serializeResult: (data) => {
     const metadata = data.aweme_list?.[0];
-    let data = new Object();
-    data.dnickname = metadata.author.nickname;
-    data.aweme_id = metadata.aweme_id;
-    data.author_id = metadata.author.uid;
-    data.avatar = metadata.author.avatar_larger.url_list[1];
-    data.title = metadata.desc;
-    data.url_original = metadata.share_url;
-    data.cover = metadata.video?.cover?.url_list.slice(0, -1);
-    data.comment = metadata.statistics.comment_count;
-    data.download = metadata.statistics.download_count;
-    data.play = metadata.statistics.play_count;
-    data.share = metadata.statistics.share_count;
-    data.collect = metadata.statistics.collect_count;
-    data.music_thumbnail = metadata.music.cover_hd.url_list[1];
-    data.music = metadata.music.play_url.uri;
-    data.video1 = metadata.video?.play_addr?.url_list.slice(0,-2);
-    data.video2 = metadata.video?.play_addr?.url_list.slice(-1);
-    data.video_wm = metadata.video?.download_addr.url_list.slice(-1);
+    let result = new Object();
+    result.dnickname = metadata.author.nickname;
+     result.aweme_id = metadata.aweme_id;
+     result.author_id = metadata.author.uid;
+     result.avatar = metadata.author.avatar_larger.url_list[1];
+     result.title = metadata.desc;
+     result.url_original = metadata.share_url;
+     result.cover = metadata.video?.cover?.url_list.slice(0, -1);
+     result.comment = metadata.statistics.comment_count;
+     result.download = metadata.statistics.download_count;
+     result.play = metadata.statistics.play_count;
+     result.share = metadata.statistics.share_count;
+     result.collect = metadata.statistics.collect_count;
+     result.music_thumbnail = metadata.music.cover_hd.url_list[1];
+     result.music = metadata.music.play_url.uri;
+     result.video1 = metadata.video?.play_addr?.url_list.slice(0,-2);
+     result.video2 = metadata.video?.play_addr?.url_list.slice(-1);
+     result.video_wm = metadata.video?.download_addr.url_list.slice(-1);
     return data;
   }
 };
