e739cc920bf124c759a995bc3bc27986e4825e46
ahmadqsyaa <rickqsyaerickch06@gmail.com>
2023-07-26 04:30:52 +0700

    yaaa

---
+++ index.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+const express = require('express')
+const axios = require('axios');
+const path = require('path')
+const fs = require('fs')
+const { getIDVideo, getOriginalUrl, serializeResult } = require(path.join(__dirname, '.','main'));
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/download', async (req, res) => {
+   const { url } = req.query
+   if(!url) return res.status(400).json({ error: 'Not Found Url!' })
+   try {
+      let link = await getOriginalUrl(url)
+      let id = getIDVideo(link)
+      let { result } = axios.get(`https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/?aweme_id=${id}`);
+      let data = serializeResult(result);
+      res.json({
+         status: true,
+         type: 'application/json',
+         data: data
+      });
+   } catch (e) {
+   return res.status(400).json({ error: e })
+      console.log(e)
+   }
+})
+app.listen(3000, () => console.log('Server started 3000'));
---
+++ main.js
@@ -0,0 +1,49 @@
+const https = require('node:https');
+module.exports = {
+  regexUrl: (url) => {
+    const regex = /^https?:\/\/([a-z]+\.|)tiktok\.com\/([\w]+|\@\D+\w+)/g;
+    return url.match(regex);
+  },
+  getIDVideo: (link) => {
+    if (!module.exports.regexUrl(link)) throw 'invalid tiktok url';
+    return link.match(/video\/([\d|\+]+)?\/?/)[1];
+  },
+  getOriginalUrl: (link) => {
+    return new Promise((resolve, reject) => {
+      const url = new URL(link);
+      const options = {
+        hostname: url.hostname,
+        path: url.pathname,
+        method: 'GET',
+      };
+      const request = https.request(options, (res) => {
+        if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) return resolve(res.headers.location);
+        resolve(link);
+      });
+      request.on('error',
+        reject);
+      request.end();
+    });
+  },
+  serializeResult: (result) => {
+    const metadata = result.aweme_list?.[0];
+    nickname = metadata.author.nickname;
+    aweme_id = metadata.aweme_id;
+    author_id = metadata.author.uid;
+    avatar = metadata.author.avatar_larger.url_list[1];
+    title = metadata.desc;
+    url_original = metadata.share_url;
+    cover = metadata.video?.cover?.url_list.slice(0, -1);
+    comment = metadata.statistics.comment_count;
+    download = metadata.statistics.download_count;
+    play = metadata.statistics.play_count;
+    share = metadata.statistics.share_count;
+    collect = metadata.statistics.collect_count;
+    music_thumbnail = metadata.music.cover_hd.url_list[1];
+    music = metadata.music.play_url.uri;
+    video1 = metadata.video?.play_addr?.url_list.slice(0,-2);
+    video2 = metadata.video?.play_addr?.url_list.slice(-1);
+    video_wm = metadata.video?.download_addr.url_list.slice(-1);
+    return result;
+  }
+};
