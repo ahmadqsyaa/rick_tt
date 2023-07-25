fca67cb95c1f492cae23464e55ffb56f8c194694
ahmadqsyaa <114840842+ahmadqsyaa@users.noreply.github.com>
2023-07-26 05:29:41 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const axios = require('axios')
-const express = require('express')
-const path = require('node:path')
-const fs = require('node:fs')
+const axios = require('axios');
+const express = require('express');
+const path = require('node:path');
+const fs = require('node:fs');
 const { getIDVideo, getOriginalUrl, serializeResult } = require(path.join(__dirname, '.','main'));
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) =>{
-   res.json({message: 'hallo bang hehe :v'})
-})
+   res.json({message: 'hallo bang hehe :v'});
+});
 app.get('/download', async (req, res) => {
    const url = req.query.url
    if(!url) return res.status(400).json({ error: 'Not Found Url!' })
    try {
       let link = await getOriginalUrl(url)
       let id = getIDVideo(link)
-      let { result } = await axios.get(`https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/?aweme_id=${id}`);
-      let datta = serializeResult(result);
+      let { data } = await axios.get(`https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/?aweme_id=${id}`);
+      let result = serializeResult(data);
       res.json({
-         datta
+         result
       });
    } catch (e) {
    res.status(400).json({ error: e })
