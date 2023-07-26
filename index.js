be6d9317f662e3942029244a048d77f51135b316
ahmadqsyaa <114840842+ahmadqsyaa@users.noreply.github.com>
2023-07-26 16:31:27 +0700

    update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/download', async (req, res) => {
       let result = serializeResult(data);
       const end = Date.time()
       res.json({
-      time: `${start - end}ms`,
+      time: `${start - end}ms`
       // type: 'application/json',
       // result
       });
