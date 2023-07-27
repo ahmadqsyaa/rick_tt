65de2c4e33e2bf6a0563d5a502e62e7acf83f9cc
ahmadqsyaa <114840842+ahmadqsyaa@users.noreply.github.com>
2023-07-27 23:23:49 +0700

    update index.js

--- index.js
+++ index.js
@@ -36,33 +36,4 @@ app.get('/download', async (req, res) => {
       console.log(e)
    }
 })
-/* */
-app.get('/download/tiktok', async (req, res) => {
-   const url = req.query.url
-   if(!url) return res.status(404).json({
-   status: 404,
-   error: 'URL not found, please insert your url'
-   })
-   try {
-      let link = await getOriginalUrl(url)
-      let id = getIDVideo(link)
-      const start = (new Date()).getTime();
-      let { data } = await axios.get(`https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/?aweme_id=${id}`);
-      //const result = serializeResult(data);
-      const end = (new Date()).getTime()
-      res.json({
-      status: 200,
-      time: `${end - start} ms`,
-      type: 'application/json',
-      data
-      });
-   } catch (e) {
-   res.status(404).json({
-   status: 404,
-   message: `video not found or ${e}`
-   })
-      console.log(e)
-   }
-})
-/* */
 app.listen(3000, () => console.log('Server started 3000'));
