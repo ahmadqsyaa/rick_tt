e6785ebffb6f3e15dd33ab99bd4734c2c07977cc
ahmadqsyaa <114840842+ahmadqsyaa@users.noreply.github.com>
2023-07-30 23:38:09 +0700

    update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { getIDVideo, getOriginalUrl, serializeResult } = require(path.join(__dirn
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("json spaces", 2);
 app.get('/', (req, res) =>{
    res.json({message: 'hallo bang hehe :v'});
 });
