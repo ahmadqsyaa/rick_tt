1c2a17b53d63d33975a1d740a98eb40b02cdd0ca
ahmadqsyaa <114840842+ahmadqsyaa@users.noreply.github.com>
2023-11-29 18:22:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -26,24 +26,7 @@ module.exports = {
     });
   },
   serializeResult: (data) => {
-    const metadata = data.aweme_list?.[0];
-    let result = new Object();
-    result.nickname = metadata.author.nickname;
-     result.aweme_id = metadata.aweme_id;
-     result.author_id = metadata.author.uid;
-     result.avatar = metadata.author.avatar_larger.url_list[1];
-     result.title = metadata.desc;
-     result.url_asli = metadata.share_url;
-     result.cover = metadata.video?.cover?.url_list[0];
-     result.comment = metadata.statistics.comment_count;
-     result.download = metadata.statistics.download_count;
-     result.play = metadata.statistics.play_count;
-     result.share = metadata.statistics.share_count;
-     result.collect = metadata.statistics.collect_count;
-     result.music_thumbnail = metadata.music.cover_hd.url_list[1];
-     result.music = metadata.music.play_url.uri;
-     result.video = metadata.video?.play_addr?.url_list;
-     result.video_wm = metadata.video?.download_addr.url_list;
-    return result;
+    
+    return data;
   }
 };
