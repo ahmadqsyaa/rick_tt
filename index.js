00580569037fc63e69fd8bf21bb74ca20511e01c
ahmadqsyaa <114840842+ahmadqsyaa@users.noreply.github.com>
2023-12-06 07:45:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,30 @@ const path = require('node:path');
 const fs = require('node:fs');
 const { getIDVideo, getOriginalUrl, serializeResult } = require(path.join(__dirname, '.','main'));
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set("json spaces", 2);
-app.get('/', (req, res) =>{
-   res.json({message: 'hallo bang hehe :v'});
-});
-app.get('/download', async (req, res) => {
-   const url = req.query.url
-   if(!url) return res.status(404).json({
-   status: 404,
-   error: 'URL not found, please insert your url'
-   })
-   try {
-      let link = await getOriginalUrl(url)
-      let id = getIDVideo(link)
-      let { data } = await axios.get(`https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/?aweme_id=${id}`);
-      const result = serializeResult(data);
-      res.json(
-      result
-      );
-   } catch (e) {
-   res.status(404).json({
-   status: 404,
-   message: `video not found or ${e}`
-   })
-      console.log(e)
-   }
-})
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+     res.render('/index');
+  })
+
+app.get('/url',async (req, res) => {
+    const text = req.query.link
+    if(!text) return res.status(404)
+    let link = await getOriginalUrl(text)
+    let id = getIDVideo(link)
+    let { data } = await axios.get(`https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/?aweme_id=${id}`);
+    const result = serializeResult(data);
+    const URL = `https://tt.rickk.biz.id/download?url=${text}`
+       res.render('/index1', {
+         datas : result.video,
+         music : result.music,
+         title : result.title,
+         play : result.play,
+         like : result.like,
+         comment : result.comment,
+         share : result.share
+     })
+}) 
+
 app.listen(3000, () => console.log('Server started 3000'));
